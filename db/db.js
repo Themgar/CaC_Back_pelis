a99f9af1ff4dfd745e53fbb9f612f60bf6dd0943
Gonzalo Vera <zalogon2002@gmail.com>
2024-07-04 09:01:09 -0300

    cambio parametros para conectar a alwaysdata

--- db/db.js
+++ db/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-host: `localhost`,
-user: `root`,
-password:  ``, 
-database: `db_pelis`
+host: `mysql-zalogon2002.alwaysdata.net`,
+user: `359814_cac_24131`,
+password:  `DQ4zcZBFB$ta4XH`, 
+database: `zalogon2002_cac_24131`
 });
+
 connection.connect((error) =>{
     if(error){
         console.error("No se pudo conectar con la base de datos");
