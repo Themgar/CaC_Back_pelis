4d985e5836924a781d6f8e00ac62a9566c6e6b70
Gonzalo Vera <zalogon2002@gmail.com>
2024-07-04 12:00:55 -0300

    agrego el use de index para interactuar con paises y peliculas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", require("./router/user.router"));
+app.use("/pais", require("./router/pais.router"));
+app.use("/pelicula", require("./router/movieRouter"));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`http://Localhost:${PORT}`));
