462d2034a011cead85d565d9d55dfdd1b3dcdfc0
PamelaViale <pamelaviale87@hotmail.com>
2024-07-04 09:57:12 -0300

    modifico funciones controller peliculas

--- controller/movieController.js
+++ controller/movieController.js
@@ -3,7 +3,7 @@
 const db = require("../db/db");
 
 const getAllMovies = (req, res) => {
-    const sql = 'SELECT * FROM movies';
+    const sql = 'SELECT * FROM peliculas';
     db.query(sql, (error, results) => {
         if (error) {
             return res.status(500).json({ error: "Intente más tarde" });
@@ -12,9 +12,10 @@ const getAllMovies = (req, res) => {
     });
 };
 
+
 const showMovieById = (req, res) => {
     const { id } = req.params;
-    const sql = "SELECT * FROM movies WHERE id = ?";
+    const sql = "SELECT * FROM peliculas WHERE id_pelicula = ?";
     db.query(sql, [id], (error, rows) => {
         if (error) {
             return res.status(500).json({ error: "Intente más tarde" });
@@ -28,22 +29,22 @@ const showMovieById = (req, res) => {
 };
 
 const createMovie = (req, res) => {
-    const { nombre, categoria, pais } = req.body;
-    const sql = "INSERT INTO movies (nombre, categoria, pais) VALUES (?, ?, ?)";
-    db.query(sql, [nombre, categoria, pais], (error, result) => {
+    const { titulo, descripcion, director, año, duracion, genero } = req.body;
+    const sql = "INSERT INTO peliculas (titulo, descripcion, director, año, duracion, genero) VALUES (?, ?, ?, ?, ?, ?)";
+    db.query(sql, [titulo, descripcion, director, año, duracion, genero], (error, result) => {
         if (error) {
             return res.status(500).json({ error: "Intente más tarde" });
         }
-        const pelicula = { id: result.insertId, nombre, categoria, pais };
+        const pelicula = { id: result.insertId, titulo, descripcion, director, año, duracion, genero };
         res.status(201).json(pelicula);
     });
 };
 
 const updateMovie = (req, res) => {
     const { id } = req.params;
-    const { nombre, categoria, pais } = req.body;
-    const sql = 'UPDATE movies SET nombre = ?, categoria = ?, pais = ? WHERE id = ?';
-    db.query(sql, [nombre, categoria, pais, id], (error, result) => {
+    const { titulo, descripcion, director, año, duracion, genero } = req.body;
+    const sql = 'UPDATE peliculas SET titulo = ?, descripcion = ?, director = ?, año = ?, duracion = ?, genero = ?  WHERE id_pelicula = ?';
+    db.query(sql, [titulo, descripcion, director, año, duracion, genero, id], (error, result) => {
         if (error) {
             return res.status(500).json({ error: "Intente más tarde" });
         }
@@ -53,10 +54,9 @@ const updateMovie = (req, res) => {
         res.json({ message: "Película actualizada con éxito" });
     });
 };
-
 const deleteMovie = (req, res) => {
     const { id } = req.params;
-    const sql = 'DELETE FROM movies WHERE id = ?';
+    const sql = 'DELETE FROM peliculas WHERE id_pelicula = ?';
     db.query(sql, [id], (error, result) => {
         if (error) {
             return res.status(500).json({ error: "Intente más tarde" });
